a9dc1411c4b851fbd4c7edd2bba845c0b64fc87b
convto <convto.jp@gmail.com>
2021-03-19 02:18:16 +0900

    parse tag from struct

--- decode.go
+++ decode.go
@@ -4,24 +4,66 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 var (
 	OrverFlowErr = errors.New("over flow")
+	UnknownType  = errors.New("unknown type")
 )
 
+type structTag struct {
+	tp             uint8
+	structFieldNum int
+}
+
+// parseTags はstructに振ってあるprotowireタグを読み取ってmapに変換する
+// mapのキーはfield_number
+func parseTags(v interface{}) (map[uint32]structTag, error) {
+	if reflect.ValueOf(v).Kind() != reflect.Ptr {
+		return nil, errors.New("struct must be a pointer")
+	}
+	rt := reflect.Indirect(reflect.ValueOf(v)).Type()
+	fieldSize := rt.NumField()
+	tags := make(map[uint32]structTag, fieldSize)
+	for i := 0; i < fieldSize; i++ {
+		f := rt.Field(i)
+		t := strings.Split(f.Tag.Get("protowire"), ",")
+		fieldNum, err := strconv.Atoi(t[0])
+		if err != nil {
+			return nil, err
+		}
+		if fieldNum > 1<<29-1 {
+			return nil, errors.New("invalid protowire structTag, largest field_number is 536,870,911")
+		}
+		tp, err := strconv.Atoi(t[1])
+		if tp > 7 {
+			return nil, errors.New("invalid protowire structTag, largest type is 7")
+		}
+		tags[uint32(fieldNum)] = structTag{tp: uint8(tp), structFieldNum: i}
+	}
+	return tags, nil
+}
+
 func Unmarshal(b []byte, v interface{}) error {
+	sts, err := parseTags(v)
+	if err != nil {
+		return fmt.Errorf("failed to parse structTag from input interface{}: %w", err)
+	}
+
 	l := len(b)
 	for l > 0 {
 		// タグは可変長バイト列形式
 		tag, n, err := readVarint(b)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to unmarshal: %w", err)
 		}
 		l -= n
 		// 仕様でtype, field_number合わせて32bitまでなので超えてたらエラー
 		if tag > math.MaxUint32 {
-			return OrverFlowErr
+			return fmt.Errorf("invalid structTag size: %w", OrverFlowErr)
 		}
 		// 下位3bitはtype, それ以外はfield_number
 		fieldNum := tag >> 3
@@ -37,7 +79,7 @@ func readVarint(b []byte) (v uint64, n int, err error) {
 	for shift := uint(0); ; shift += 7 {
 		// 64bitこえたらoverflow
 		if shift >= 64 {
-			return 0, 0, OrverFlowErr
+			return 0, 0, fmt.Errorf("failed to read varint: %w", OrverFlowErr)
 		}
 		// 対象のbyteの下位7bitを読み取ってvにつめていく
 		target := b[n]
---
+++ decode_test.go
@@ -0,0 +1,76 @@
+package protowire
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parseTags(t *testing.T) {
+	type tagTest struct {
+		Age  int    `protowire:"1,0"`
+		Name string `protowire:"2,2"`
+		Max  string `protowire:"536870911,7"`
+	}
+	type invalidFieldNumber struct {
+		Age int `protowire:"536870912,0"`
+	}
+	type invalidType struct {
+		Age int `protowire:"1,8"`
+	}
+
+	tests := []struct {
+		name    string
+		v       interface{}
+		want    map[uint32]structTag
+		wantErr bool
+	}{
+		{
+			name: "タグの値を読み取れる",
+			v:    &tagTest{},
+			want: map[uint32]structTag{
+				1: {
+					tp:             0,
+					structFieldNum: 0,
+				},
+				2: {
+					tp:             2,
+					structFieldNum: 1,
+				},
+				536870911: {
+					tp:             7,
+					structFieldNum: 2,
+				},
+			},
+		},
+		{
+			name:    "vがポインタじゃないとエラー",
+			v:       tagTest{},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "field numberが上限より大きいとエラー",
+			v:       &invalidFieldNumber{},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "field numberが上限より大きいとエラー",
+			v:       &invalidType{},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseTags(tt.v)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseTags() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseTags() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
