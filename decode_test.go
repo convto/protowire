6381b7e68710eac7a834f94cc285dcbcf028400f
convto <convto.jp@gmail.com>
2021-03-25 01:13:18 +0900

    refactor test

--- decode_test.go
+++ decode_test.go
@@ -1,47 +1,82 @@
 package protowire
 
 import (
-	"encoding/hex"
 	"reflect"
 	"testing"
+
+	"github.com/convto/protowire/testdata"
+	"github.com/golang/protobuf/proto"
 )
 
 func TestUnmarshal(t *testing.T) {
-	testVarintBin, _ := hex.DecodeString("08b96010b292041801")
+	testVarintBin, _ := proto.Marshal(&testdata.TestVarint{
+		Int32:   12345,
+		Int64:   67890,
+		Boolean: true,
+	})
 	type testVarint struct {
 		Int32   int32 `protowire:"1,0,int32"`
 		Int64   int64 `protowire:"2,0,int64"`
 		Boolean bool  `protowire:"3,0,bool"`
 	}
-	testVarintZigzagBin, _ := hex.DecodeString("08f1c00110e3a408")
+
+	testVarintZigzagBin, _ := proto.Marshal(&testdata.TestVarintZigzag{
+		Sint32: -12345,
+		Sint64: -67890,
+	})
 	type testVarintZigzag struct {
 		Sint32 int32 `protowire:"1,0,sint32"`
 		Sint64 int64 `protowire:"2,0,sint64"`
 	}
-	testLengthDelimitedBin, _ := hex.DecodeString("0a18e38193e3828ce381afe381a6e38199e381a8e381a0e382881206ffeeddccbbaa")
+
+	testLengthDelimitedBin, _ := proto.Marshal(&testdata.TestLengthDelimited{
+		Str:   "これはてすとだよ",
+		Bytes: []byte{0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA},
+	})
 	type testLengthDelimited struct {
 		Str   string `protowire:"1,2,string"`
 		Bytes []byte `protowire:"2,2,bytes"`
 	}
-	test64BitBin, _ := hex.DecodeString("09393000000000000011cef6feffffffffff191bde8342cac0f33f")
+
+	test64BitBin, _ := proto.Marshal(&testdata.Test64Bit{
+		Fixed64:  12345,
+		Sfixed64: -67890,
+		Double:   1.23456789,
+	})
 	type test64Bit struct {
 		Fixed64  uint64  `protowire:"1,1,fixed64"`
 		Sfixed64 int64   `protowire:"2,1,sfixed64"`
 		Double   float64 `protowire:"3,1,double"`
 	}
-	test32BitBin, _ := hex.DecodeString("0d3930000015cef6feff1d52069e3f")
+
+	test32BitBin, _ := proto.Marshal(&testdata.Test32Bit{
+		Fixed32:  12345,
+		Sfixed32: -67890,
+		Float:    1.23456789,
+	})
 	type test32Bit struct {
 		Fixed32  uint32  `protowire:"1,5,fixed32"`
 		Sfixed32 int32   `protowire:"2,5,sfixed32"`
 		Float    float32 `protowire:"3,5,float"`
 	}
-	testEmbedBin, _ := hex.DecodeString("0a1808c79fffffffffffffff0110ceedfbffffffffffff01180112260a1ce38193e3828ce381afe381a6e38199e381a8e381a0e38288f09f909b1206ffeeddccbbaa1a1b09393000000000000011cef6feffffffffff191bde8342cac0f33f")
+
+	testEmbedBin, _ := proto.Marshal(&testdata.TestEmbed{
+		EmbedVarint: &testdata.TestVarint{
+			Int32:   -12345,
+			Int64:   -67890,
+			Boolean: true,
+		},
+		EmbedLengthDelimited: &testdata.TestLengthDelimited{
+			Str:   "これはてすとだよ🐛",
+			Bytes: []byte{0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA},
+		},
+		Embed64Bit: &testdata.Test64Bit{
+			Fixed64:  12345,
+			Sfixed64: -67890,
+			Double:   1.23456789,
+		},
+	})
 	type testEmbed struct {
-		TestVarint          testVarint          `protowire:"1,2,embed"`
-		TestLengthDelimited testLengthDelimited `protowire:"2,2,embed"`
-		Test64Bit           test64Bit           `protowire:"3,2,embed"`
-	}
-	type testEmbedHasPtrFields struct {
 		TestVarint          *testVarint          `protowire:"1,2,embed"`
 		TestLengthDelimited *testLengthDelimited `protowire:"2,2,embed"`
 		Test64Bit           *test64Bit           `protowire:"3,2,embed"`
@@ -122,29 +157,6 @@ func TestUnmarshal(t *testing.T) {
 				v: &testEmbed{},
 			},
 			want: &testEmbed{
-				TestVarint: testVarint{
-					Int32:   -12345,
-					Int64:   -67890,
-					Boolean: true,
-				},
-				TestLengthDelimited: testLengthDelimited{
-					Str:   "これはてすとだよ🐛",
-					Bytes: []byte{0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA},
-				},
-				Test64Bit: test64Bit{
-					Fixed64:  12345,
-					Sfixed64: -67890,
-					Double:   1.23456789,
-				},
-			},
-		},
-		{
-			name: "Embedの検証バイナリで、bindする型のフィールドがポインタ",
-			args: args{
-				b: testEmbedBin,
-				v: &testEmbedHasPtrFields{},
-			},
-			want: &testEmbedHasPtrFields{
 				TestVarint: &testVarint{
 					Int32:   -12345,
 					Int64:   -67890,
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/convto/protowire
 
 go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.1
+	google.golang.org/protobuf v1.26.0
+)
